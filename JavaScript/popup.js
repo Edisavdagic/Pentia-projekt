2ce4e367aadee3cd17ae61b2ba112b910075d3d6
Edisavdagic <119938495+Edisavdagic@users.noreply.github.com>
2026-03-07 15:27:06 +0100

    filer slettet

--- JavaScript/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("show-popup").addEventListener("click", function() {
-    var popup = document.querySelector(".popup-wrap");
-    popup.style.display = "block";
-  });
-
-  document.querySelector(".popup-btn").addEventListener("click", function() {
-    var popup = document.querySelector(".popup-wrap");
-    popup.style.display = "none";
-  });
