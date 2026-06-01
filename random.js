f11d8e2c7ad30e7e00767b96e0569804f895c478
Edisavdagic <119938495+Edisavdagic@users.noreply.github.com>
2026-06-01 15:22:07 +0200

    fejl rettet

--- random.js
+++ random.js
@@ -20,3 +20,18 @@ const beskeder = [
 const tilfældigBesked = beskeder[Math.floor(Math.random() * beskeder.length)];
 
 console.log(tilfældigBesked);
+
+let held = calculateRandom(1, 100);
+console.log(`Dit held i dag er: ${held}`);  
+
+function visTilfældigBesked() {
+  const beskeder = [
+    "Du fandt en hemmelig skat 🪙",
+    "En kat stjæler din sandwich 🐈",
+    "Du får +10 held i dag ✨",
+    "En mystisk dør åbner sig 🚪" ,
+  ];
+  const tilfældigBesked = beskeder[Math.floor(Math.random() * beskeder.length)];
+  console.log(tilfældigBesked);
+}
+
